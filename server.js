9d2af31f855a71294c6725ae82175cd625da65af
NinaPajonk <50745190+NinaPajonk@users.noreply.github.com>
2021-05-08 20:13:21 +0200

    basic routing, ejs, 404 error handling

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const app = express();
-const multer = require('multer');
+// const multer = require('multer');
 const port = 3000;
+const path = require('path');
+const dogs = ['David', 'Inju', 'Nina', 'Anna']
+
 
 console.log('hello world')
 
-
 // statische pagina's
-app.use(express.static('public'))
+app.use(express.static('/public/'))
+
+// template engine 
+app.set('views', 'view')
+app.set('view engine', 'ejs')
 
 // routes
-app.get('*', function(req, res){
-res.sendFile(path.join(__dirname, '/404.html'))
-})
-
-app.get('/login', function(req, res) {
-res.sendFile(path.join(__dirname, '/index.html'))
+app.get('/', function(req, res) {
+res.render('login', {hondenNamen: dogs})
 
 })
 app.get('/register', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
     
+    })
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '/public/404.html'))
     })
 
 //Server check
