9f70ecebc235b4c21e934bcc04c74efe8d7a59e2
NinaPajonk <50745190+NinaPajonk@users.noreply.github.com>
2021-05-19 23:58:32 +0200

    .find, new mongo db connection

--- server.js
+++ server.js
@@ -2,15 +2,50 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-const path = require('path'); 
+const path = require('path');
 const mongoose = require('mongoose');
 const dogs = require('./dogs.json')
 
 // dot env
-require('dotenv').config()
+// require('dotenv').config()
+
+// Mongoose Schema
+const Schema = mongoose.Schema;
+
+const dogscollectionSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    ras: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    kleur: {
+        type: String,
+        required: true
+    },
+    prijs: {
+        type: String,
+        required: true
+    }
+}, {
+    collection: 'dogscollection'
+});
+
+const dogsdb = mongoose.model('dogscollection', dogscollectionSchema);
+
 
 // html inladen
-const html = dogs.map(dog=> {
+const html = dogs.map(dog => {
     return `
 <article class="dog">
 <a href="#${dog.id}">
@@ -26,59 +61,61 @@ const html = dogs.map(dog=> {
 // MongoDB Database
 const db = mongoose.connection;
 
-const url = 'mongodb+srv://NPajonk:Nina%40pajonk25@cluster0-ofs74.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-mongoose.connect(url, {'useNewUrlParser': true, 'useUnifiedTopology': true});
-db.collection('dogscollection');
+const url = 'mongodb+srv://Walvishaai18:Walvishaai18@cluster0.ofs74.mongodb.net/databasedogs?retryWrites=true&w=majority'
+mongoose.connect(url, {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true
+});
 
-db.collection('dogscollection').find({})
 
-// Mongoose Schema
-const Schema = mongoose.Schema;
 
-const dogscollectionSchema = new Schema({
-  id: { type: String, required: true },
-  name: { type: String, required: true },
-  ras: { type: String, required: true },
-  gender: { type: String, required: true },
-  kleur: { type: String, required: true },
-  prijs: { type: Number, required: true }
-}, { collection: 'dogscollection' });
-
-const dogsdb = mongoose.model('dogscollection', dogscollectionSchema);
-
-dogsdb.find({ id: req.params.id });
+dogsdb.find({
+    kleur: 'wit'
+}), (err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+};
 
 
 // bodyparser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // statische pagina's
 app.use(express.static(__dirname + '/public/'))
 
 
 // template engine 
-app.set('views', 'view')
+app.set('views', 'view');
 app.set('view engine', 'ejs')
 
 // routes (dynamische pagina's)
-app.get('/', function(req, res) {
-res.render('overview', {hondenNamen: dogs})
+app.get('/', function (req, res) {
+    res.render('overview', {
+        hondenNamen: dogs
+    })
 
 })
 // test html inladen
-app.get('/test', function(req, res) {
-    res.render('test', {dog: html })
-    
+app.get('/test', function (req, res) {
+    res.render('test', {
+        dog: html
     })
 
-app.get('/register', function(req, res) {
+})
+
+app.get('/register', function (req, res) {
     res.render('register')
-    
-    })
+
+})
 // handling error 404 static
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/404.html'))
-    })
+})
 
 
 //Server check
