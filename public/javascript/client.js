6a86f7ff5f010b5aa2012aa59b87fc3700aaf587
NinaPajonk <50745190+NinaPajonk@users.noreply.github.com>
2021-06-15 01:02:19 +0200

    header animation js

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -9,17 +9,37 @@ for (let i = 0; i < splitText.length; i++) {
 let char = 0;
 let timer = setInterval(onTick, 50);
 
-function onTick(){
+function onTick() {
     const span = text.querySelectorAll('span')[char];
     span.classList.add('fade');
     char++;
 
-    if(char === splitText.length){
+    if (char === splitText.length) {
+        complete();
+        return;
+    }
 
-    
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+    }
+}
 
-    complete();
-    return;
-}}
- 
 /* Text Animation Tutorial bron: https://www.youtube.com/watch?v=GUEB9FogoP8 */
+
+/* Form validation */
+const form = document.querySelector("form");
+const nameInput = document.querySelector(
+    'input[name="naam"]'
+);
+
+const validateInputs = () => {
+    if (!nameInput.value) {
+        nameInput.classList.add("invalid");
+        nameInput.nextElementSibling.classList.remove("hidden");
+    }
+};
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+validateInputs();
+});
