6ba13312e5eb6204a0ca67272b412ec5587e4f9e
NinaPajonk <50745190+NinaPajonk@users.noreply.github.com>
2021-06-22 02:34:32 +0200

    javascript form validation prijs en naam

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,68 +1,69 @@
-const text = document.querySelector(".fancy");
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent = "";
-for (let i = 0; i < splitText.length; i++) {
-    text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
-
-let char = 0;
-let timer = setInterval(onTick, 50);
-
-function onTick() {
-    const span = text.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++;
-
-    if (char === splitText.length) {
-        complete();
-        return;
-    }
-
-    function complete() {
-        clearInterval(timer);
-        timer = null;
-    }
-}
 /* Text Animation Tutorial bron: https://www.youtube.com/watch?v=GUEB9FogoP8 */
+// const text = document.querySelector(".fancy");
+// const strText = text.textContent;
+// const splitText = strText.split("");
+// text.textContent = "";
+// for (let i = 0; i < splitText.length; i++) {
+//     text.innerHTML += "<span>" + splitText[i] + "</span>";
+// }
+
+// let char = 0;
+// let timer = setInterval(onTick, 50);
+
+// function onTick() {
+//     const span = text.querySelectorAll('span')[char];
+//     span.classList.add('fade');
+//     char++;
+
+//     if (char === splitText.length) {
+//         complete();
+//         return;
+//     }
+
+//     function complete() {
+//         clearInterval(timer);
+//         timer = null;
+//     }}
+
 
 /* Form validation https://www.youtube.com/watch?v=ps0-JAQENXI */
 
 
 const form = document.querySelector("form[name='toevoeg-form']");
 const nameInput = document.querySelector("input[name='naam']");
+const noNameInputError = document.querySelector(".error")
+const symbolenInputError = document.querySelector(".symbolen")
+const prijsInput = document.querySelector("input[name='prijs']");
+const noPrijsInput = document.querySelector(".noPrijs")
 
-nameInput.isValid = () => !!nameInput.value;
 
-const inputFields = [nameInput];
+nameInput.addEventListener('blur', (e) => {
+    e.preventDefault()
 
-let shouldValidate = false;
-let isFormValid = false;
+    if (nameInput.value === '') {
+        console.log(noNameInputError)
+        noNameInputError.style.display = "block"
 
-const validateInputs = () => {
-    console.log("we are here");
-    if (!shouldValidate) return;
-
-    isFormValid = true;
-    inputFields.forEach((input) => {
-        input.classList.remove("invalid");
-        input.nextElementSibling.classList.add("hide");
-
-        if (!input.isValid()) {
-            input.classList.add("invalid");
-            isFormValid = false;
-            input.nextElementSibling.classList.remove("hide");
-        }
-    });
-};
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    shouldValidate = true;
-    validateInputs();
-    if (isFormValid) {
-
+    } else if (!/[A-zÀ-ž- ]+/.test(nameInput.value)) {
+        // console.log('Denk aan de patterns')
+        symbolenInputError.style.display = "block"
+    } else {
+        noNameInputError.style.display = "none"
+        symbolenInputError.style.display = "none"
     }
-});
 
-inputFields.forEach((input) => input.addEventListener("input", validateInputs));
+})
+
+prijsInput.addEventListener('blur', (e) => {
+    e.preventDefault()
+
+    if(prijsInput.value === ''){
+        console.log('wat is je prijs')
+        noPrijsInput.style.display = "block"
+    }
+    else {
+        noPrijsInput.style.display = "none"
+    }
+})
+
+
